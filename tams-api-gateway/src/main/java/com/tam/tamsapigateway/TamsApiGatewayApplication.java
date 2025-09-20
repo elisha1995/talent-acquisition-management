2e131dd5f3a30ce9414034f9162ad02dc14f4de5
Elisha Samuel Gyamfi <elishasamuel01@gmail.com>
2025-09-20 22:54:48 +0000

    feat: add axon dependencies and configure eureka clients

--- tams-api-gateway/src/main/java/com/tam/tamsapigateway/TamsApiGatewayApplication.java
+++ tams-api-gateway/src/main/java/com/tam/tamsapigateway/TamsApiGatewayApplication.java
@@ -2,8 +2,10 @@ package com.tam.tamsapigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class TamsApiGatewayApplication {
 
     public static void main(String[] args) {
